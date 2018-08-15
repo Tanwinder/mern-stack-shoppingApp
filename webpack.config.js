31afffea1a595e2bac1d1630351e490dd6d775b8
tkmam0t <tanwinder.sidhu@kohls.com>
2018-08-15 14:38:21 -0500

    first commit project setup

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: "./index.js",
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+    },
+    context: path.resolve(__dirname, 'src'),
+    plugins: [
+     new HtmlWebpackPlugin({
+            template: 'index.html'
+        })
+    ]
+}
