30dfdae1594a7daa25325ea8d87fe1e064eab389
Tanwinder Sidhu <sidhu@Gurpreets-Air.attlocal.net>
2018-08-16 23:59:17 -0500

    connect to database

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://Tan:Tan12345@ds153380.mlab.com:53380/mern_shoppingapp'
+}
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express()
+
+// body parser middleware
+app.use(bodyParser.json());
+
+// db config
+const db = require('./config/keys').mongoURI;
+
+// connect mongodb
+mongoose
+  .connect(db)
+  .then( () => console.log('MongoDb connected..'))
+  .catch( err => console.log(err));
+
+  const port = process.env.PORT || 5000;
+
+  app.listen( port, () => console.log(`Server started at port ${port}`));
